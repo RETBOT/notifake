303e252a8516642cec865d01251d6ad96c3e70fb
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:15:17 -0600

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+# by: RETBOT 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
+# by: RETBOT 
