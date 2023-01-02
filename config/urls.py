d36affe081160c3a8f9bb40a1398081e4baad656
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:15:48 -0600

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-
+# by: RETBOT 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cuentas/', include('django.contrib.auth.urls')),
@@ -25,3 +25,4 @@ urlpatterns = [
     path('', include('paginas.urls')),
     # path('', TemplateView.as_view(template_name='inicio.html'), name='inicio'),
 ]
+# by: RETBOT 
