db098484bccb7983b280d73b2c7c898a8616a073
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:18:38 -0600

    Update models.py

--- cuentas/models.py
+++ cuentas/models.py
@@ -2,7 +2,8 @@ from tokenize import blank_re
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-
+# by: RETBOT 
 class UsuarioPers(AbstractUser):
     edad = models.PositiveIntegerField(null=True, blank = True)
 
+# by: RETBOT 
